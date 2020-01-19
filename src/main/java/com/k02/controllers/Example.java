2d8760595559caf08b041dba96a8945ab034a532
hoangvanvuong <vuonghoangvan7@gmail.com>
2020-01-19 13:27:14 +0900

    em vẫn đang bị lỗi gì đó vẫn chưa thể chạy được a xem rồi bảo giúp em với ạ

--- src/main/java/com/k02/controllers/Example.java
+++ src/main/java/com/k02/controllers/Example.java
@@ -5,13 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.k02.entity.Lophoc;
 import com.k02.entity.User;
+import com.k02.service.LophocService;
 import com.k02.service.UserService;
 
 @Controller
 public class Example {
 	@Autowired
 	private UserService userSrv;
+	@Autowired
+	private LophocService lophocSrv;
 	
 	@RequestMapping("/hello")
 	@ResponseBody
@@ -22,4 +26,17 @@ public class Example {
 		userSrv.save(user);
 		return "Hello Spring Boot";
 	}
+	@RequestMapping("/lophoc")
+	@ResponseBody
+	public String lophoc() {
+		Lophoc lophoc = new Lophoc();
+		lophoc.setMaLopHoc("l1");
+		lophoc.setTenLopHoc("lop1");
+		lophoc.setTongSoSinhVien(2);
+		lophocSrv.save(lophoc);
+		return  " nhap thong tin thanh cong";	
+	}
+	
+	
+
 }
