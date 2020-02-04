ba7e1f5a70bfba0fa76fc3ecda18c24b674ebf84
binhtrandev <binhtrandev@gmail.com>
2020-02-04 18:48:14 +0700

    no message

---
+++ src/main/java/com/k02/config/SpringSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.k02.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+// http://docs.spring.io/spring-boot/docs/current/reference/html/howto-security.html
+// Switch off the Spring Boot security configuration
+//@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AccessDeniedHandler accessDeniedHandler;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+	// roles admin allow to access /admin/**
+	// roles user allow to access /user/**
+	// custom 403 access denied handler
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable().authorizeRequests()
+				.antMatchers("/webjars/**").permitAll()
+				.antMatchers("/css/**", "/js/**", "/templates/**").permitAll()
+				.antMatchers("/favicon.ico").permitAll()
+				.antMatchers("/", "/home", "/about", "/logout", "/registration").permitAll()
+				.antMatchers("/admin/**").hasAnyRole("ADMIN")
+				.antMatchers("/user/**").hasAnyRole("USER")
+				.anyRequest().authenticated().and()
+				.formLogin().loginPage("/login").permitAll()
+				.and().logout().permitAll()
+				.and().exceptionHandling()
+				.accessDeniedHandler(accessDeniedHandler);
+	}
+
+	@Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+}
---
+++ src/main/java/com/k02/controllers/DefaultController.java
@@ -0,0 +1,48 @@
+package com.k02.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String home1() {
+        return "/home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "/home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("username: " + auth.getName());
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
+    }
+
+}
