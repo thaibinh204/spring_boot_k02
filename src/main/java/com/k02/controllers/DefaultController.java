4d5cbb3fd2ca0f97b4aa4ecf1fe6f9ace35d6f35
nguyen-thi-hai-yen154 <yenkhtn154@gmail.com>
2020-02-09 15:39:34 +0900

    refactor source

--- src/main/java/com/k02/controllers/DefaultController.java
+++ src/main/java/com/k02/controllers/DefaultController.java
@@ -1,21 +1,11 @@
 package com.k02.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.k02.entity.Customer;
-import com.k02.service.CustomerService;
 
 
 @Controller
@@ -23,7 +13,6 @@ public class DefaultController {
 
     @GetMapping("/")
     public String home1() {
-    	String test = "";
         return "/home";
     }
 
