4297d5b08d1bc7b33d2550f8f1ff18cab0cb0f5d
hoangvanvuong <vuonghoangvan7@gmail.com>
2020-02-09 12:48:48 +0900

    Customer

--- src/main/java/com/k02/controllers/CustomerController.java
+++ src/main/java/com/k02/controllers/CustomerController.java
@@ -1,5 +1,32 @@
 package com.k02.controllers;
 
-public class CustomerController {
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+public class CustomerController {
+	
+	@RequestMapping(value = { "/customerList" }, method = RequestMethod.GET) 
+	public String customerList(Model model){
+		
+//		model.addAttribute("customer", customerList);
+		
+		return "customerList";
+		
+	}
+	@RequestMapping(value = { "/detail" }, method = RequestMethod.GET)
+	public String detail(Model model) {
+		
+		return "detail";
+	}
+	@RequestMapping(value = { "/edit" }, method = RequestMethod.GET)
+	public String edit(Model model) {
+		
+		return "edit";
+	}
+	@RequestMapping(value = { "/delete" }, method = RequestMethod.GET)
+	public String delete(Model model) {
+		
+		return "delete";
+	}
 }
