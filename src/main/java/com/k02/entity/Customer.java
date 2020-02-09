416a3fb661f51889d767f92622920506d8634dd9
tohientrang <tohientrang81@gmail.com>
2020-02-09 09:28:07 +0900

    entity customer

---
+++ src/main/java/com/k02/entity/Customer.java
@@ -0,0 +1,92 @@
+package com.k02.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+
+
+public class Customer {
+	@Id
+	@Column(name ="customer_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name ="customer_name")
+	private String customerName;
+	
+	@Column(name ="contact_name")
+	private String contactName;
+	
+	@Column(name ="address")
+	private String address;
+	
+	@Column(name ="city")
+	private String city;
+	
+	@Column( name="postalcode")
+	private String postalCode;
+	
+	@Column( name="country")
+	private String country;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
